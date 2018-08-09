3ea265d48e37b8108cacc06f6b01e797b5e0b476
Miguel Savignano <miguel.savignano@the-cocktail.com>
2018-08-09 17:56:19 +0200

    try use js doc

--- src/react-render-pages.js
+++ src/react-render-pages.js
@@ -6,6 +6,10 @@ import ReactDOM from 'react-dom'
 //   render() { return <h1>This component render in "#example-domId"</h1>}
 // }
 // ReactDOMRender("example-domId", Example)
+/**
+ * render component in dom id.
+ * @param {string} dom_id - dom id.
+ */
 const ReactDOMRender = (dom_id, Component) => {
   document.addEventListener('DOMContentLoaded', () => {
     const element = document.getElementById(dom_id)
@@ -15,12 +19,15 @@ const ReactDOMRender = (dom_id, Component) => {
   })
 }
 
-// import { render } from 'react-render-pages'
-// @render("example-domId")
-// class Example extends React.Component {
-//   render() { return <h1>This component render in "#example-domId"</h1>}
-// }
-
+/**
+ * Decorator function.
+ * import { render } from 'react-render-pages'
+ * @render("example-domId")
+ * class Example extends React.Component {
+ *   render() { return <h1>This component render in "#example-domId"</h1>}
+ * }
+ * @param {string} dom_id - dom id.
+ */
 const render = (dom_id) => {
   return (target, property, descriptor) => {
     ReactDOMRender(dom_id, target)
@@ -31,7 +38,12 @@ const render = (dom_id) => {
 // const Example = renderIn("example-domId", (props) =>
 //   <h1>This component render in "#example-domId"</h1>
 // ))
-
+/**
+ * render component in dom id.
+ * @param {string} dom_id - dom id.
+ * @param {function} Component - react-component.
+ * @return {function} Component - react-component
+ */
 const renderIn = (dom_id, Component) => {
   ReactDOMRender(dom_id, Component)
   return Component
