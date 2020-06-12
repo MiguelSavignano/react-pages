7a6fcf3b86af20b464db6e2ff4109b170ea2e699
Miguel Savignano <migue.masx@gmail.com>
2020-06-12 23:51:31 +0200

    fix get element props from html

--- src/react-render-pages.js
+++ src/react-render-pages.js
@@ -8,13 +8,13 @@ import ReactDOM from 'react-dom'
 // ReactDOMRender("example-domId", Example)
 /**
  * render component in dom id.
- * @param {string} dom_id - dom id.
+ * @param {string} domId - dom id.
  */
-const ReactDOMRender = (dom_id, Component) => {
+const ReactDOMRender = (domId, Component) => {
   document.addEventListener('DOMContentLoaded', () => {
-    const element = document.getElementById(dom_id)
+    const element = document.getElementById(domId)
     if (!element) return false
-    const props = JSON.parse(element.dataset.props) || {}
+    const props = JSON.parse(element.dataset.props || '{}')
     ReactDOM.render(React.createElement(Component, props, null), element)
   })
 }
@@ -26,11 +26,11 @@ const ReactDOMRender = (dom_id, Component) => {
  * class Example extends React.Component {
  *   render() { return <h1>This component render in "#example-domId"</h1>}
  * }
- * @param {string} dom_id - dom id.
+ * @param {string} domId - dom id.
  */
-const render = (dom_id) => {
+const render = (domId) => {
   return (target, property, descriptor) => {
-    ReactDOMRender(dom_id, target)
+    ReactDOMRender(domId, target)
   }
 }
 
@@ -40,12 +40,12 @@ const render = (dom_id) => {
 // ))
 /**
  * render component in dom id.
- * @param {string} dom_id - dom id.
+ * @param {string} domId - dom id.
  * @param {function} Component - react-component.
  * @return {function} Component - react-component
  */
-const renderIn = (dom_id, Component) => {
-  ReactDOMRender(dom_id, Component)
+const renderIn = (domId, Component) => {
+  ReactDOMRender(domId, Component)
   return Component
 }
 
